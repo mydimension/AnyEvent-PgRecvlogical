b2424743775d073ccb7039153406a515d0665094
William Cox <mydimension@gmail.com>
2017-10-08 16:17:32 -0400

    functional; time to document

--- lib/AnyEvent/PgRecvlogical.pm
+++ lib/AnyEvent/PgRecvlogical.pm
@@ -1,5 +1,299 @@
-use strict;
-use warnings;
 package AnyEvent::PgRecvlogical;
+
+# ABSTRACT: perl port of pg_recvlogical
+
+use Moo;
+use DBI;
+use DBD::Pg 3.7.0 ':async';
+use AnyEvent;
+use Promises backend => ['AnyEvent'], qw(deferred);
+use Types::Standard ':all';
+use Try::Tiny;
+use Carp 'croak';
+
+use constant {
+    AWAIT_INTERVAL     => 1,
+    USECS             => 1_000_000,
+    PG_MIN_VERSION    => 9_04_00,
+    PG_MIN_NOEXPORT   => 10_00_00,
+    PG_STATE_DUPEOBJ  => '42710',
+    PG_EPOCH_DELTA    => 946_684_800,
+    XLOGDATA          => 'Aq>3a*',
+    PRIMARY_HEARTBEAT => 'Aq>2b',
+    STANDBY_HEARTBEAT => 'Aq>4b',
+};
+
+use namespace::clean;
+
+my $DBH = (InstanceOf ['DBI::db'])->create_child_type(
+    constraint => sub {
+        $_->{Driver}->{Name} eq 'Pg'
+          and $_->{pg_server_version} >= PG_MIN_VERSION
+          and $_->{Name} =~ /replication=/;
+    },
+    message => sub {
+        my $parent_check = (InstanceOf['DBI::db'])->validate($_);
+        return $parent_check if $parent_check;
+        return "$_ is not a DBD::Pg handle" unless $_->{Driver}->{Name} eq 'Pg';
+        return "$_ is connected to an old postgres version ($_->{pg_server_version} < 9.4.0)" unless $_->{pg_server_version} >= PG_MIN_VERSION;
+        return "$_ is not a replication connection: $_->{Name}" unless $_->{Name} =~ /replication=/;
+    }
+);
+
+my $LSNStr = Str->where(sub { m{[0-9A-F]{1,8}/[0-9A-F]{1,8}} })
+  ->plus_coercions(Int() => sub { sprintf '%X/%X', (($_ >> 32) & 0xffff_ffff), ($_ & 0xffff_ffff) });
+
+my $LSN = Int->plus_coercions(
+    $LSNStr => sub {
+        my ($h, $l) = map { hex } split m{/}; ($h << 32) | $l;
+    }
+);
+
+has dbname   => (is => 'ro', isa => Str, required  => 1);
+has host     => (is => 'ro', isa => Str, predicate => 1);
+has port     => (is => 'ro', isa => Int, predicate => 1);
+has username => (is => 'ro', isa => Str, predicate => 1);
+has password => (is => 'ro', isa => Str, predicate => 1);
+has slot     => (is => 'ro', isa => Str, required  => 1);
+
+has dbh => (is => 'lazy', isa => $DBH, clearer => 1, init_arg => undef);
+has do_create_slot => (is => 'ro', isa => Bool,    default => 0);
+has slot_exists_ok => (is => 'ro', isa => Bool,    default => 0);
+has reconnect      => (is => 'ro', isa => Bool,    default => 1);
+has heartbeat      => (is => 'ro', isa => Int,     default => 10);
+has plugin         => (is => 'ro', isa => Str,     default => 'test_decoding');
+has options        => (is => 'ro', isa => HashRef, default => sub { {} });
+has autoflush      => (is => 'ro', isa => Bool,    default => 1);
+has startpos     => (is => 'rwp', isa => $LSN, default => 0, coerce  => 1);
+has received_lsn => (is => 'rwp', isa => $LSN, default => 0, clearer => 1, init_arg => undef);
+has flushed_lsn  => (is => 'rwp', isa => $LSN, default => 0, clearer => 1, init_arg => undef);
+
+has on_message => (is => 'ro', isa => CodeRef, required => 1);
+has on_error => (is => 'ro', isa => CodeRef, default => sub { \&croak });
+
+has _fh_watch => (is => 'lazy', isa => Ref, clearer => 1);
+has _timer    => (is => 'lazy', isa => Ref, clearer => 1);
+
+sub _dsn {
+    my $self = shift;
+
+    my %dsn = (replication => 'database', client_encoding => 'sql_ascii');
+    foreach (qw(host port dbname)) {
+        my $x = "has_$_";
+        next if $self->can($x) and not $self->$x;
+
+        $dsn{$_} = $self->$_;
+    }
+
+    return 'dbi:Pg:' . join q{;}, map { "$_=$dsn{$_}" } sort keys %dsn;
+}
+
+sub _build_dbh {
+    my $self = shift;
+    return DBI->connect(
+        $self->_dsn,
+        ($self->has_username ? $self->username : ''),
+        ($self->has_password ? $self->password : ''),
+    );
+}
+
+sub _build__fh_watch {
+    my $self = shift;
+    return AE::io $self->dbh->{pg_socket}, 0, sub { $self->_read_copydata };
+}
+
+sub _build__timer {
+    my $self = shift;
+    return AE::timer $self->heartbeat, $self->heartbeat, sub { $self->_heartbeat };
+}
+
+sub start {
+    my $self = shift;
+
+    $self->_post_init(
+        deferred {
+            shift->chain(sub { $self->identify_system }, sub { $self->create_slot }, sub { $self->start_replication });
+        }
+    );
+}
+
+sub _post_init {
+    my ($self, $d) = @_;
+
+    return $d->then(
+        sub {
+            $self->_fh_watch;
+            $self->_timer;
+        },
+        $self->on_error,
+    );
+}
+
+sub identify_system {
+    my $self = shift;
+    $self->dbh->do('IDENTIFY_SYSTEM', { pg_async => PG_ASYNC });
+    return _async_await($self->dbh);
+}
+
+sub create_slot {
+    my $self = shift;
+
+    return deferred->resolve unless $self->do_create_slot;
+
+    my $dbh = $self->dbh;
+    $dbh->do(
+        sprintf(
+            'CREATE_REPLICATION_SLOT %s LOGICAL %s%s',
+            $dbh->quote_identifier($self->slot),
+            $dbh->quote_identifier($self->plugin),
+            ($dbh->{pg_server_version} >= PG_MIN_NOEXPORT ? ' NOEXPORT_SNAPSHOT' : '')
+        ),
+        { pg_async => PG_ASYNC }
+    );
+
+    return _async_await($dbh)->catch(
+        sub {
+            croak @_ unless $dbh->state eq PG_STATE_DUPEOBJ and $self->slot_exists_ok;
+        }
+    );
+}
+
+sub _option_string {
+    my $self = shift;
+
+    my @opts;
+    while (my ($k, $v) = each %{ $self->options }) {
+        push @opts, $self->dbh->quote_identifier($k);
+        defined $v and $opts[-1] .= sprintf ' %s', $self->dbh->quote($v);
+    }
+
+    return @opts ? sprintf('(%s)', join q{, }, @opts) : q{};
+}
+
+sub start_replication {
+    my $self = shift;
+
+    $self->dbh->do(
+        sprintf(
+            'START_REPLICATION SLOT %s LOGICAL %s%s',
+            $self->dbh->quote_identifier($self->slot),
+            $LSNStr->coerce($self->startpos),
+            $self->_option_string
+        ),
+    );
+}
+
+sub _read_copydata {
+    my $self = shift;
+
+    my $n = $self->dbh->pg_getcopydata_async(my $msg);
+
+    return if $n == 0;    # nothing waiting
+
+    if ($n == -1) {
+        AE::postpone { $self->_handle_disconnect };
+        return;
+    }
+
+    if ($n == -2) {
+        # error reading
+        $self->on_error->('could not read COPY data: ' . $self->dbh->errstr);
+    }
+
+    my $type = substr $msg, 0, 1;
+
+    if ('k' eq $type) {
+        # server keepalive
+        my (undef, $lsnpos, $ts, $reply) = unpack PRIMARY_HEARTBEAT, $msg;
+
+        # only interested in the request-reply bit
+        if ($reply) {
+            AE::postpone { $self->_heartbeat };
+        }
+
+        return;
+    }
+
+    unless ('w' eq $type) {
+        $self->on_error->("unrecognized streaming header: '$type'");
+    }
+
+    my (undef, $startlsn, $endlsn, $ts, $record) = unpack XLOGDATA, $msg;
+
+    $self->_set_received_lsn($startlsn);
+
+    if ($self->autoflush) {
+        $self->on_message->($record);
+        $self->_set_flushed_lsn($startlsn) if $startlsn > $self->flushed_lsn;
+    } else {
+        $self->on_message->(
+            $record,
+            sub {
+                $self->_set_flushed_lsn($startlsn) if $startlsn > $self->flushed_lsn;
+            }
+        );
+    }
+
+    # do it again until $n == 0
+    my $w; $w = AE::timer 0, 0, sub { undef $w; $self->_read_copydata };
+}
+
+sub stop {
+    my $self = shift;
+
+    $self->_clear_fh_watch;
+    $self->_clear_timer;
+    $self->clear_dbh;
+}
+
+sub _handle_disconnect {
+    my $self = shift;
+
+    $self->stop;
+
+    return unless $self->reconnect;
+
+    $self->_startpos($self->flushed_lsn);
+    $self->clear_received_lsn;
+    $self->clear_flushed_lsn;
+
+    $self->_post_init($self->start_replication);
+}
+
+sub _heartbeat {
+    my ($self, $req_reply) = @_;
+    $req_reply = !!$req_reply || 0;
+
+    my $status = pack STANDBY_HEARTBEAT, 'r',     # receiver status update
+      $self->received_lsn,                        # last WAL received
+      $self->flushed_lsn,                         # last WAL flushed
+      0,                                          # last WAL applied
+      int((AE::now - PG_EPOCH_DELTA) * USECS),    # ms since 2001-01-01
+      $req_reply;                                 # request heartbeat
+
+    $self->dbh->pg_putcopydata($status);
+}
+
+sub _async_await {
+    my ($dbh) = @_;
+
+    my $d = deferred;
+
+    # no async operation in progress
+    return $d->reject if $dbh->{pg_async_status} == 0;
+
+    my $w; $w = AE::timer 0, AWAIT_INTERVAL, sub {
+        return unless $dbh->pg_ready;
+        try {
+            $d->resolve($dbh->pg_result);
+        }
+        catch {
+            $d->reject($_);
+        };
+        undef $w;
+    };
+
+    return $d->promise;
+}
 
 1;
---
+++ t/01_recv.t
@@ -0,0 +1,62 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use Test::More;
+use Test::PostgreSQL;
+use AnyEvent;
+use File::Basename;
+use File::Spec;
+use Promises backend => ['AnyEvent'], qw(deferred);
+
+use ok 'AnyEvent::PgRecvlogical';
+
+my $t_dir = File::Spec->rel2abs(dirname(__FILE__));
+my $pg_hba_conf = File::Spec->join($t_dir, 'pg_hba.conf');
+
+my $pg = eval {
+    Test::PostgreSQL->new(extra_postmaster_args =>
+          "-c hba_file='$pg_hba_conf' -c wal_level=logical -c max_wal_senders=1 -c max_replication_slots=1");
+}
+  or plan skip_all => "cannot create test postgres database: $Test::PostgreSQL::errstr";
+
+my $control = DBI->connect($pg->dsn, 'postgres');
+
+my $end_cv = AE::cv;
+
+my $recv = new_ok(
+    'AnyEvent::PgRecvlogical' => [
+        dbname         => 'test',
+        host           => '127.0.0.1',
+        port           => $pg->port,
+        username       => 'postgres',
+        slot           => 'test',
+        options        => { 'skip-empty-xacts' => 1 },
+        do_create_slot => 1,
+        slot_exists_ok => 1,
+        on_message     => sub { pass "got message: @_"; $end_cv->send if $_[0] =~ /payload\[text\]:'qwerty'/ },
+        on_error => sub { fail $_[0]; $end_cv->croak(@_) },
+    ],
+    'pg_recvlogical'
+);
+
+$end_cv->cb(sub { $recv->stop });
+
+ok $recv->dbh, 'connected';
+
+$recv->start->done(
+    sub {
+        pass 'replication started';
+
+        $control->do('create table test_tbl (id int primary key, payload text)');
+        $control->do('insert into test_tbl (id, payload) values (?, ?)', undef, 1, 'qwerty');
+    },
+    sub {
+        fail 'replication started';
+        diag @_;
+    }
+);
+
+$end_cv->recv;
+
+done_testing;
